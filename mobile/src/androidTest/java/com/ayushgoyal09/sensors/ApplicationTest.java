c1ad3b9c948153143000eaa049b62313c438c774
Ayush Goyal <agoyal@tacpoint.com>
2016-09-01 15:40:46 -0700

    initial commit

---
+++ mobile/src/androidTest/java/com/ayushgoyal09/sensors/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ayushgoyal09.sensors;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
